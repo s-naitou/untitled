bd5079ed45674d68ef53c88ab4d4c2aabe99ec62
so-naitou <0llnsll0@gmail.com>
2023-07-16 19:55:30 +0900

    なんとなくPUSH

--- python/himatubusi.py
+++
@@ -1,9 +0,0 @@
-tomatu = 1
-taka = 1
-if taka > tomatu:
-    print("たかちゃんはでかい！")
-elif taka < tomatu:
-    print("たかちゃんはちいさい！")
-    print("たかちゃんはぶす！")
-else:
-    print("両方ぶす！")
---
+++ python/story.py
@@ -0,0 +1,11 @@
+import syokugyou
+print("ーーーーーードラゴンクエストーーーーーー")
+name = input("あなたの名前は？＞")
+print("職業はナニニシマスカ。以下から職業名を入力してください")
+job = input("戦士、魔法使い、盗賊、タンク")
+if job == "戦士":
+    usable = syokugyou.soldier
+    print(f"あなたは{usable}を獲得しました！それでは楽しい旅に行ってらっしゃい！")
+elif job == "戦士":
+    usable = syokugyou.soldier
+    print(f"あなたは{usable}を獲得しました！それでは楽しい旅に行ってらっしゃい！")
